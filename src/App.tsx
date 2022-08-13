36ea0b64388e0c21816cfe32280b055b533317dd
André Geraldo Guimarães Pinto <italiabk@gmail.com>
2022-08-12 22:31:29 -0400

    build: styled components example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 
+import { Button } from './components/Button';
 
 export function App() {
   return (
-   <div>
-      <h1>Hello World</h1>
-   </div>
+  <>
+    <Button variant='primary' />
+    <Button variant='secondary' />
+    <Button variant='danger' />
+    <Button variant='success' />
+    <Button />
+  </>
   )
 }
 
