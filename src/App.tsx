64ce6973c5eab571167b11c67b32ab4b71170f33
André Geraldo Guimarães Pinto <italiabk@gmail.com>
2022-08-16 08:37:09 -0400

    fix: eslint patern on files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
- 
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/themes/global';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
-  return ( 
-  <ThemeProvider theme={defaultTheme}>
-    <Button variant='primary' />
-    <Button variant='secondary' />
-    <Button variant='danger' />
-    <Button variant='success' />
-    <Button />
-    <GlobalStyle/> 
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
