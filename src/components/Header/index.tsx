84367f1c620cd8be70dfa8af9585aaa512b69540
André Geraldo Guimarães Pinto <italiabk@gmail.com>
2022-08-23 22:42:14 -0400

    build: header style and icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import { HeaderContainer } from './styles'
-
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logoIgnite} alt="" title="" />
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Timer">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
